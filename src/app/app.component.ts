fb03593cfeac986361bb6a331b90f315adf3d65d
MacobJagyar <jacobmagyar95@gmail.com>
2022-10-27 22:09:56 -0400

    finished pizza lab

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuItem } from './menuitem';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'menu';
+
+  menu: MenuItem[] = [
+   
+    {
+      Item: "Pepperoni Pizza",
+      Category: "Pizza",
+      Price: 15
+    },
+    {
+      Item: "Pineapple Pizza (crowd favorite)",
+      Category: "Pizza",
+      Price: 13.50
+    },
+    {
+      Item: "Cheese Pizza",
+      Category: "Pizza",
+      Price: 12
+    },
+    {
+      Item: "Wings",
+      Category: "Side",
+      Price: 6.50
+    },
+    {
+      Item: "Salad",
+      Category: "Side",
+      Price: 4.49
+    },
+    {
+      Item: "Cookies",
+      Category: "Dessert",
+      Price: 3
+    },
+    {
+      Item: "Cinnamon Sticks",
+      Category: "Dessert",
+      Price: 7
+    }
+  ]
 }
